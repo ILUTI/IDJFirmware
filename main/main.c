7145bd7a58b62d6d22b85209f35421f6e37dbd2a
educhii <echivalan@launion.com.gt>
2025-08-12 15:20:22 -0600

    Match de ROMs con unidades
    Se agregó la función y lógica para match de roms con unidades

--- main/main.c
+++ main/main.c
@@ -3,6 +3,33 @@
 #include "ds2431.h"
 #include "driver/i2c.h"
 
+//Tabla para mapeo
+typedef struct {
+    uint64_t rom;
+    const char *unidad;
+} dispositivo_info_t;
+
+dispositivo_info_t dispositivos_registrados[] = {
+    {0xEC000048F3EA902D, "T0603-0001"},
+    {0x5D000048F3FFF42D, "T0603-0002"},
+    {0x28000048F45F022D, "T0603-0003"},
+    {0x84000048F49AA22D, "T0603-0004"},
+    {0xF0000048F45E392D, "T0603-0005"},
+    {0xB6000048F472F92D, "T0603-0006"}
+};
+
+//Funcion para mapeo
+const size_t num_dispositivos_registrados = sizeof(dispositivos_registrados) / sizeof(dispositivo_info_t);
+
+const char* buscar_unidad_por_rom(uint64_t rom) {
+    for (size_t i = 0; i < num_dispositivos_registrados; i++) {
+        if (dispositivos_registrados[i].rom == rom) {
+            return dispositivos_registrados[i].unidad;
+        }
+    }
+    return "Desconocido";
+}
+
 #define I2C_MASTER_SCL_IO 10  // Cambia aquí el pin SCL (IO10)
 #define I2C_MASTER_SDA_IO 11  // Cambia aquí el pin SDA (IO11)
 #define I2C_MASTER_NUM I2C_NUM_0
@@ -49,17 +76,20 @@ void app_main(void) {
     }
 
     //Detecta solo un dispositivo 1-Wire
-    /*uint64_t rom_code = 0;
+    /*
+    uint64_t rom_code = 0;
     err = ds2482_search_rom(&rom_code);
     if (err == ESP_OK) {
         printf("Dirección ROM del dispositivo 1-Wire: 0x%016llX\n", rom_code);
     } else {
         printf("Error buscando la dirección ROM del dispositivo 1-Wire.\n");
-    }*/
+    }
+    */
 
+    //Escaneo de ROM's
+    /*
     while (1)
     {
-        /* code */
     uint64_t roms[5];
     size_t found = 0;
     err = ds2482_search_rom_all(roms, 5, &found);
@@ -71,6 +101,19 @@ void app_main(void) {
     } else {
             printf("Error buscando dispositivos 1-Wire.\n");
         }
-        vTaskDelay(pdMS_TO_TICKS(5000)); // Espera 1 segundo antes de la siguiente iteración
+        vTaskDelay(pdMS_TO_TICKS(5000)); // Espera 5 segundos antes de la siguiente iteración
+    }
+    */
+
+    //Mapeo de unidades y match con ROM's
+    while(1){
+        uint64_t roms[5];
+        size_t found = 0;
+        err = ds2482_search_rom_all(roms, 5, &found);
+        for (size_t i = 0; i < found; i++) {
+        const char *unidad = buscar_unidad_por_rom(roms[i]);
+        printf("Dispositivo %zu ROM: 0x%016llX - Unidad: %s\n", i + 1, roms[i], unidad);
+        }
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
